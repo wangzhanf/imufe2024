de8079c9af00af1a49470586e3ce5b0ab7a5ba19
wangzhanf <wangzhanf@126.com>
2024-10-16 10:27:09 +0800

    20241016-002

--- vue-project-front/src/views/Home.vue
+++ vue-project-front/src/views/Home.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts" name="Home">
+    import { ref } from 'vue';
+    import axios  from "axios";
 
+    //准备数据结构
+    let imgSrc = ref("https://cdn2.thecatapi.com/images/4lr.gif");
+    let imgTitle = ref("标题");
+
+    //不断刷新请求网络数据
+    setInterval(() => {//匿名函数，箭头函数，lambda表达式
+        let a = axios.get("https://api.thecatapi.com/v1/images/search?size=full").then((res)=>{
+            // console.log(res); 
+            imgSrc.value = res.data[0].url;          
+            imgTitle.value = res.data[0].id;          
+        });
+    }, 50000);//50秒刷新
 </script>
 <template>
-    News Home
+    <div class="zone">
+        <img v-bind:src="imgSrc" />
+        <h3>{{ imgTitle }}</h3>
+    </div>
 </template>
+<style scoped>
+    .zone{
+        width: 80%;
+        margin: 4em auto;
+    }
+    .zone img{
+        width: 90%;
+        border-radius: 10px;
+    }
+</style>
